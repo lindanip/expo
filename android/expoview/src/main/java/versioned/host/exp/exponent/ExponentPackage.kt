2c8b770808bb2a3016c0b989f9218f6fda21ba64
lukmccall <kosmatylukasz@gmail.com>
2022-10-06 17:56:14 +0200

    [iOS][Android] Update `react-native-svg` to `13.3.0`

--- android/expoview/src/main/java/versioned/host/exp/exponent/ExponentPackage.kt
+++ android/expoview/src/main/java/versioned/host/exp/exponent/ExponentPackage.kt
@@ -7,6 +7,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
+import com.horcrux.svg.SvgPackage
 import com.shopify.reactnative.flash_list.ReactNativeFlashListPackage
 import com.shopify.reactnative.skia.RNSkiaPackage
 import com.swmansion.rnscreens.RNScreensPackage
@@ -37,7 +38,6 @@ import versioned.host.exp.exponent.modules.api.components.reactnativestripesdk.S
 import versioned.host.exp.exponent.modules.api.components.sharedelement.RNSharedElementModule
 import versioned.host.exp.exponent.modules.api.components.sharedelement.RNSharedElementPackage
 import versioned.host.exp.exponent.modules.api.components.slider.ReactSliderPackage
-import versioned.host.exp.exponent.modules.api.components.svg.SvgPackage
 import versioned.host.exp.exponent.modules.api.components.pagerview.PagerViewPackage
 import versioned.host.exp.exponent.modules.api.components.webview.RNCWebViewModule
 import versioned.host.exp.exponent.modules.api.components.webview.RNCWebViewPackage
